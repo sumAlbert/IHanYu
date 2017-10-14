6647016e692f3257e7972d7633928abe8f49afb5
Quinn Norris <sei_qnz2015@126.com>
2017-10-14 09:51:25 +0800

    Revert "out"
    This reverts commit 8ee486694e2b575259add49abf701b1a92c92ae0.

--- out/production/IHanYu/res/js/teacher3.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dell2 on 2017/5/14.
- */
-$(document).ready(function(){
-    $(".info-check-icon").click(function(){
-        $(this).toggleClass("info-check-icon-active");
-        $(this).toggleClass("info-check-icon-inactive");
-    });
-});
