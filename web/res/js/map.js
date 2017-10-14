14f83b7d4bbbeeb114b4127952b7431b91f43347
sumAlbert <982675088@qq.com>
2017-10-14 22:42:53 +0800

    add a input box in teacher2.jsp for teacher's English name

--- web/res/js/map.js
+++ web/res/js/map.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     map = new AMap.Map("map", {
         resizeEnable: true,
         zoom: 10,
@@ -9,20 +8,22 @@ $(document).ready(function () {
     map.on('mouseup',function () {
         lng=map.getCenter().lng;
         lat=map.getCenter().lat;
-        mapCenterChange();
+        var page_name=$("#map").attr("name");
+        mapCenterChange(page_name);
     });
 
     /**
      *  当前地图的中心点改变，将其发送给后端
      */
-    function mapCenterChange(){
+    function mapCenterChange(page_name){
         $.ajax({
             type: "post",
             url: "/test",
             timeout: 800000,
             data : {
                 lng: lng,
-                lat: lat
+                lat: lat,
+                page_name: page_name
             },
             success : function (data) {
                 console.log(data.errorCode);
